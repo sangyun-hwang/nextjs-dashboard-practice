e9bacd13d999bb9a669af2da2395da435f4f9b19
sangyun-hwang <s_infinite@naver.com>
2026-09-10 23:58:44 +0900

    ch11: add invoice creation, editing and deletion

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -20,5 +20,80 @@ export default function EditInvoiceForm({
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
-  return <form action={updateInvoiceWithId}>{/* ... */}</form>;
+  return (
+    <form action={updateInvoiceWithId}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <div className="mb-4">
+          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+            Choose customer
+          </label>
+          <select
+            id="customer"
+            name="customerId"
+            defaultValue={invoice.customer_id}
+            className="block w-full rounded-md border border-gray-200 p-2 text-sm"
+          >
+            <option value="" disabled>
+              Select a customer
+            </option>
+            {customers.map((customer) => (
+              <option key={customer.id} value={customer.id}>
+                {customer.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+            Choose an amount
+          </label>
+          <input
+            id="amount"
+            name="amount"
+            type="number"
+            step="0.01"
+            defaultValue={invoice.amount}
+            className="block w-full rounded-md border border-gray-200 p-2 text-sm"
+          />
+        </div>
+
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Set the invoice status
+          </legend>
+          <div className="flex gap-4">
+            <label className="flex items-center gap-2">
+              <input
+                name="status"
+                type="radio"
+                value="pending"
+                defaultChecked={invoice.status === 'pending'}
+              />
+              Pending
+            </label>
+            <label className="flex items-center gap-2">
+              <input
+                name="status"
+                type="radio"
+                value="paid"
+                defaultChecked={invoice.status === 'paid'}
+              />
+              Paid
+            </label>
+          </div>
+        </fieldset>
+      </div>
+
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/invoices"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Edit Invoice</Button>
+      </div>
+    </form>
+  );
 }
